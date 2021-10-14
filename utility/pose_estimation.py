e852ba2226d5820e5d9bbd8d79f7e0680b452cea
Josiah Coad <josiah@coad.net>
2021-10-14 01:37:45 -0500

    some nonworking pose client calls

--- utility/pose_estimation.py
+++ utility/pose_estimation.py
@@ -1,6 +1,10 @@
-from transformation_matrix import TransformationMatrix
+from .transformation_matrix import TransformationMatrix
 import numpy as np
-import copy
+import requests
+from collections import namedtuple
+from utility.pose_data import TRANFORM_BOUNDS, get_bolt_rgbd, get_random_transform
+
+ImageResponse = namedtuple('ImageResponse', ['color', 'depth', 'image_id'])
 
 
 def rmseT(estimate, truth):
@@ -14,34 +18,48 @@ def rmseT(estimate, truth):
 
 
 def evaluate_random(estimator):
-    transform_truth = TransformationMatrix.make_random()
-    transformed = copy.deepcopy(bolt_rgbd).transform(transform_truth)
+    transform_truth = get_random_transform()
+    transformed = get_bolt_rgbd(transform_truth)
     transform_estimate = estimator(transformed)
     return rmseT(transform_truth, transform_estimate)
 
 
 def evaluate_batch(estimator, count=100):
-    return np.mean([evaluate_random(target) for _ in range(count)])
+    return np.mean([evaluate_random(estimator) for _ in range(count)])
 
 
 def _get_image():
-    response = requests.get(endpoint + '/transformed_bolt_rgbd').data
-    return response
+    endpoint = 'http://138.197.220.122:8090/'
+    response = requests.get(endpoint + 'pose/transformed_bolt_rgbd/').json()
+    print(response)
+    return ImageResponse(
+        np.array(response[0]),
+        np.array(response[1]),
+        response[2]
+    )
 
 
 def _post_estimates(estimates, myname):
-    res = requests.post(endpoint + '/estimate', {
-        'estimates': estimate,
-        'myname': myname
+    endpoint = 'http://138.197.220.122:8090/'
+    res = requests.post(endpoint + 'pose/estimate/', {
+        'estimates': estimates,
+        'name': myname
     })
     return res.json()['mean_rmseT']
 
 
-def evaluate_remote(estimator, myname):
-    from threading import Pool
-    threads = Pool(100)
-    estimates = threads.map(_get_image) \
-        .map(lambda res: {'estimate': estimator(res.image), 'id': res.id}) \
-        .reduce(np.concatenate)
-    mean_rmseT = _post_estimates(estimates, myname)
+def make_submission(estimator):
+    import os
+    username = os.getenv('DISCORD_USERNAME')
+    if not username:
+        raise Exception(
+            'Please set the environment variable "DISCORD_USERNAME"')
+
+    estimates = []
+    for _ in range(10):
+        img = _get_image()
+        estimate = estimator(img)
+        estimates.append(
+            {'estimate': estimate.to_list(), 'image_id': img.image_id})
+    mean_rmseT = _post_estimates(estimates, username)
     return mean_rmseT
